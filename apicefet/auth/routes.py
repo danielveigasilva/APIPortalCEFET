30d8f4227e8b8dfed14d4cf2a3991c3476700717
Thalles Koester <thallesdella@gmail.com>
2020-02-14 14:49:53 -0300

    Update auth route

--- apicefet/auth/routes.py
+++ apicefet/auth/routes.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 import apicefet.auth as auth
 
 auth_bp = Blueprint('token', __name__)
-auth_bp.add_url_rule('/<string:user>/<string:passwd>', 'user', auth.auth)
+auth_bp.add_url_rule('', 'user', auth.auth, methods=['POST'])
