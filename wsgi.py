6c6f0de6823d21a12ecc6d1e53596904ce9f7b5a
Thalles Koester <thallesdella@gmail.com>
2020-02-14 13:46:54 -0300

    Add new entry point

---
+++ wsgi.py
@@ -0,0 +1,9 @@
+from apicefet import create_app
+import os
+
+app = create_app()
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    # app.run(debug=False, host='0.0.0.0', port=port)  # para prod, ative aqui!
+    app.run(debug=True, host='127.0.0.1', port=port)  # para dev, ative aqui!
