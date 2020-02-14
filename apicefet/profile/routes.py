829562e11ea3128ff00c77876fad1b858662ccaa
Thalles Koester <thallesdella@gmail.com>
2020-02-14 12:56:01 -0300

    Update router handle function name

--- apicefet/profile/routes.py
+++ apicefet/profile/routes.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 import apicefet.profile as profile
 
 profile_bp = Blueprint('profile', __name__)
-profile_bp.add_url_rule('', 'user', profile.perfilDados)
-profile_bp.add_url_rule('/geral', 'geral', profile.perfilDadosGerais)
-profile_bp.add_url_rule('/foto', 'photo', profile.perfilFoto)
+profile_bp.add_url_rule('', 'user', profile.profile_data)
+profile_bp.add_url_rule('/geral', 'geral', profile.profile_data_all)
+profile_bp.add_url_rule('/foto', 'photo', profile.profile_photo)
