7d5258987ab5b60bdf7c25ee422c5e30d4e84621
Thalles Koester <thallesdella@gmail.com>
2020-02-14 13:31:24 -0300

    Update reports handle function name

--- apicefet/reports/routes.py
+++ apicefet/reports/routes.py
@@ -2,5 +2,5 @@ from flask import Blueprint
 import apicefet.reports as reports
 
 reports_bp = Blueprint('reports', __name__)
-reports_bp.add_url_rule('', 'list', reports.lista_relatorios)
-reports_bp.add_url_rule('/<path:url>', 'generate', reports.geraRelatorio)
+reports_bp.add_url_rule('', 'list', reports.reports_list)
+reports_bp.add_url_rule('/<path:url>', 'generate', reports.reports_generate)
