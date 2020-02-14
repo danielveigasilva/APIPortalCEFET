df2e6ec73b80684996c5474413779c7fce3f2c01
Thalles Koester <thallesdella@gmail.com>
2020-02-14 15:10:06 -0300

    Changing back to production

--- wsgi.py
+++ wsgi.py
@@ -5,5 +5,5 @@ app = create_app()
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    # app.run(debug=False, host='0.0.0.0', port=port)  # para prod, ative aqui!
-    app.run(debug=True, host='127.0.0.1', port=port)  # para dev, ative aqui!
+    app.run(debug=False, host='0.0.0.0', port=port)  # para prod, ative aqui!
+    # app.run(debug=True, host='127.0.0.1', port=port)  # para dev, ative aqui!
