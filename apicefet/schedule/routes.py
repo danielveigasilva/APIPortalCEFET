d5664f1ff9d08af08f801e5b97cf072624396292
Thalles Koester <thallesdella@gmail.com>
2020-02-14 13:35:22 -0300

    Update schedule handle function name

--- apicefet/schedule/routes.py
+++ apicefet/schedule/routes.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 import apicefet.schedule as schedule
 
 schedule_bp = Blueprint('token', __name__)
-schedule_bp.add_url_rule('', 'time', schedule.horarios)
+schedule_bp.add_url_rule('', 'time', schedule.schedules)
